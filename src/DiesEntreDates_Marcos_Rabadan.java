008bc947841b248e9e650271d56deb3a409a2ac2
MarcosRaCo <marcosrabcob@gmail.com>
2020-12-18 18:00:43 +0100

    Inicial Dates

--- src/DiesEntreDates_Marcos_Rabadan.java
+++ src/DiesEntreDates_Marcos_Rabadan.java
@@ -7,67 +7,44 @@
  * Description:
  **/
 public class DiesEntreDates_Marcos_Rabadan extends CalcularDiesEntreDates{
-    /** Array que contiene los dias de cada mes **/
-    private final int[] diasMeses = new int[]{31,28,31,30,31,30,31,31,30,31,30,31};
-
 
     @Override
     protected int diesMes(int mes) {
-        //Le pasamos un mes como parametro Restamos 1 para cuando sea enero (el primer mes, que ocupa la posicion 0 del array, por eso restamos el 1 )
-        return this.diasMeses[mes - 1];
+        return 0;
     }
 
     @Override
     protected int diesMesInicial(DataXS dataXS) {
-        //Restamos a los dias(de los meses) el dia que le pasamos por parametro para saber los dias que faltan para acabar el mes inicial
-        //Llamamos al metodo anterior
-        return this.diesMes(dataXS.mes) - dataXS.dia;
+        return 0;
     }
-    //Dias que quedan hasta final de mes
+
     @Override
     protected int diesMesDesti(DataXS dataXS) {
-        return dataXS.dia;
+        return 0;
     }
-    //Dias que quedan hasta final de año
+
     @Override
     protected int diesResteAnyInicial(DataXS datainicial) {
-        int resto = this.diesMesInicial(datainicial);
-        for (int i = datainicial.mes; i < 12; i++) {
-            resto = resto + this.diasMeses[i];
-        }
-        return resto - this.diesMesInicial(datainicial);
+        return 0;
     }
-    //Dias que quedan hasta principio de año
+
     @Override
     protected int diesResteAnyDesti(DataXS datadesti) {
-        int resto = this.diesMesDesti(datadesti);
-        for (int i = 0; i < datadesti.mes - 1; i++) {
-            resto += diasMeses[i];
-        }
-        return resto - this.diesMesDesti(datadesti);
+        return 0;
     }
-    //Devolvera dias entre la primera fecha y la segunda
+
     @Override
     protected int diesNumAnysComplets(DataXS datainicial, DataXS datadesti) {
-        int diesAnyComplets = 0;
-        for (int i = datainicial.any + 1; i < datainicial.any + (datadesti.any - datainicial.any); i++){
-            if (!anyDeTraspas(i)) diesAnyComplets += 365;
-        }
-        return diesAnyComplets;
+        return 0;
     }
-    //Para sumar 1 cuando el año sea bisiesto
+
     @Override
     protected int numDiesPerAnysdeTraspas(DataXS datainicial, DataXS datadesti) {
-        int diesAnysDeTraspas = 0;
-        for (int i = datainicial.any + 1; i < datainicial.any + (datadesti.any - datainicial.any); i++){
-            if (anyDeTraspas(i)) diesAnysDeTraspas += 366;
-        }
-        return diesAnysDeTraspas;
+        return 0;
     }
-    //Bisiestos
+
     @Override
     protected boolean anyDeTraspas(int any) {
-        this.diasMeses[1] = 29;
-        return (any % 400 == 0) || ((any % 4 == 0) && !(any % 100 == 0));
+        return false;
     }
 }
--- src/TestCalcularDias.java
+++ src/TestCalcularDias.java
@@ -8,14 +8,6 @@
  **/
 public class TestCalcularDias {
     public static void main(String[] args) {
-        DiesEntreDates_Marcos_Rabadan dias = new DiesEntreDates_Marcos_Rabadan();
-        try {
-            DataXS fecha1 = new DataXS("24/08/2000");
-            DataXS fecha2 = new DataXS("18/12/2020");
-            System.out.println(dias.nombreDiesTotals(fecha1, fecha2));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
 
     }
 }
